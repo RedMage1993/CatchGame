3a0c77d1a8b04b20fb600f4e85b586f29ef68246
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-08-05 15:58:52 -0700

    hat moving to mouse position

---
+++ Assets/Scripts/HatController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatController : MonoBehaviour {
+
+    public Camera cam;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        // Direct rigidbody2d usage deprecated
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is not called once per frame
+    // Called once per physics timestep (can be set in settings)
+	void FixedUpdate () {
+        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition); // Of mouse
+        Vector3 targetPosition = new Vector3(rawPosition.x, 0.0f, 0.0f); // Of hat
+        rb2d.MovePosition(targetPosition);
+	}
+}
