c009bdb45989249e722b08229ad19728edc85a1d
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-08-24 16:17:33 -0700

    restart button connected to restartgame func

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+
+    public void RestartGame()
+    {
+        // Application.LoadLevel obsolete.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
