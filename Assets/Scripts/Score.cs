cebd9cda28d8c79411a4c0be4d436fef2c668dff
Fritz Ammon <ammonf@unlv.nevada.edu>
2016-08-23 22:18:23 -0700

    update score on collision

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    public int ballValue;
+
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        UpdateScoreText();
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        score += ballValue;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = "Score:\n" + score;
+    }
+}
