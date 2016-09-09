ab4cb35fb758b03f29402d86375ec07f0d8d931e
RedMage1993 <redmage1993@gmail.com>
2016-09-08 23:48:12 -0700

    spawning bombs too now

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class GameController : MonoBehaviour {
     public Camera cam;
     private Renderer rend;
-    public GameObject ball;
+    public GameObject[] balls; // Now holding ball and bomb
     public float timeLeft;
     public Text timerText;
     public GameObject gameOverText;
@@ -22,7 +22,7 @@ public class GameController : MonoBehaviour {
     void Start()
     {
         // Use on ball game object, otherwise will grab object attached to.
-        rend = ball.GetComponent<Renderer>(); // Have to do this to get bounds property
+        rend = balls[0].GetComponent<Renderer>(); // Have to do this to get bounds property
 
         limits = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0.0f, 0.0f));
         limits.x -= rend.bounds.extents.x; // Subtract half of ball width (extents is half of box)
@@ -72,7 +72,7 @@ public class GameController : MonoBehaviour {
             // No rotation
             Quaternion spawnRot = Quaternion.identity;
 
-            Instantiate(ball, spawnPos, spawnRot);
+            Instantiate(balls[Random.Range(0, balls.Length)], spawnPos, spawnRot);
 
             yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
         }
